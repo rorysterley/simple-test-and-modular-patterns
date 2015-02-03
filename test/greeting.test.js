1c466aa3ebb7a56ce93c2e57e2f0c09e514c02b6
Rory Sterley <royrsterley@gmail.com>
2015-02-03 10:26:20 -0800

    Add test for CLI

--- test/greeting.test.js
+++ test/greeting.test.js
@@ -4,7 +4,11 @@ var expect = require('chai').expect;
 var greeting = require('../lib/greeting');
 
 describe('greeting', function() {
-  it('should return hello Einstein', function() {
+  it('should return "hello Einstein"', function() {
     expect(greeting.greet('Einstein')).to.eql('hello Einstein');
+  });
+  it('should return "hello Octocat" using process.argv', function() {
+    process.argv = 'Octocat';
+    expect(greeting.greet(process.argv)).to.eql('hello Octocat');
   });
 });
